7b2b1683cfb4b3cef387f7fa297343d317c8f02a
hanzzoo <kou435@gmail.com>
2025-07-19 14:08:10 +0900

    fix(app): DATABASE_URLの文字セットをutf8mb4に設定し、get_all_products関数のレスポンスをJSON形式に変更

--- app/main.py
+++ app/main.py
@@ -1,13 +1,13 @@
 from fastapi import FastAPI, Depends
+from fastapi.responses import JSONResponse
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
 from typing import AsyncGenerator
 import os
 
 from app.repository.ProductRepository import ProductRepository
 from app.schemas import ProductResponse
-# from app.schemas import ProductResponse as ProductsSchema
 
-DATABASE_URL = os.getenv("DATABASE_URL", "mysql+aiomysql://user:user_password@db:3306/my_database")
+DATABASE_URL = os.getenv("DATABASE_URL", "mysql+aiomysql://user:user_password@db:3306/my_database?charset=utf8mb4")
 engine = create_async_engine(DATABASE_URL, echo=True)
 async_session = async_sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
 
@@ -28,9 +28,13 @@ async def root():
 async def health_check():
     return {"status": "healthy"}
 
-@app.get("/products", tags=["products"])
-async def get_all_products(db: AsyncSession = Depends(get_db)):
+@app.get("/products", tags=["products"], response_model=list[ProductResponse])
+async def get_all_products(db: AsyncSession = Depends(get_db)) -> JSONResponse:
     async with db.begin():
         repo = ProductRepository(db)
         result = await repo.get_all_products()
-        return [ProductResponse.model_validate(product) for product in result]
+        json_data = [ProductResponse.model_validate(product).model_dump() for product in result]
+        return JSONResponse(
+            content={ "products": json_data },
+            media_type="application/json; charset=utf-8"
+        )
