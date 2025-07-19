fd5db5d3048a72a340e790463ab2429944723cb1
hanzzoo <kou435@gmail.com>
2025-07-20 08:57:21 +0900

    feat(cart): カートインスタンスを取得するメソッドを追加し、既存のカートがある場合はそのインスタンスを返す機能を実装

--- app/repository/CartRepository.py
+++ app/repository/CartRepository.py
@@ -1,18 +1,36 @@
 
 import uuid
 import datetime
+
+from sqlalchemy import select
 from app.domain.Cart import Cart
 from sqlalchemy.ext.asyncio import AsyncSession
 
 class CartRepository:
   def __init__(self, db: AsyncSession) -> None:
     self.db = db
+  
+  async def _get_instance(self, user_id: str) -> str | None:
+    try:
+      query = select(Cart).where(Cart.user_id == user_id)
+      result = await self.db.execute(query)
+      cart = result.scalars().one_or_none()
+      if cart:
+        return str(cart.instance)
+      else:
+        return None
+    except Exception as e:
+      raise ValueError(f"not found cart instance, {e}")
     
   async def create_cart(self, user_id: str) -> str:
     #TODO: user_idからカートのインスタンスを検索し既存のCartがあれば、そちらに保存
     # (user_idはログインユーザーであれば、DB / 未ログインユーザーであればセッションIDとなる)
     #TODO: ログインユーザーであれば、UserRepositoryにバリデーションメソッドを追加し、そちらで判定処理
     try:
+      has_created_instance = await self._get_instance(user_id)
+
+      if has_created_instance:
+        return has_created_instance
       instance = uuid.uuid4().hex
       created_at = datetime.datetime.now(datetime.timezone.utc)
       cart = Cart(instance=instance, user_id=user_id, created_at=created_at)
