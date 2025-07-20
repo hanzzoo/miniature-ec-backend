6946813f88027e523a81de02a3160011ba54e974
hanzzoo <kou435@gmail.com>
2025-07-20 21:31:24 +0900

    feat(user): ユーザーモデルを追加

---
+++ app/domain/User.py
@@ -0,0 +1,16 @@
+import datetime
+from sqlalchemy import Column, String
+from sqlalchemy.orm import declarative_base
+
+Base = declarative_base()
+class User(Base):
+    __tablename__ = "user"
+
+    user_id = Column(String, primary_key=True, index=True)
+    created_at = Column(String, default=datetime.datetime.now, index=True)
+    token = Column(String, unique=True, index=True)
+
+    def __init__(self, user_id: str, created_at: datetime.datetime, token: str):
+        self.user_id = user_id
+        self.created_at = created_at
+        self.token = token
