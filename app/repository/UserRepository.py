c2500baf47dfe93d8ddabc758d2d0df9455bfcc4
hanzzoo <kou435@gmail.com>
2025-07-20 23:17:15 +0900

    feat(repository): register_userメソッドからtokenを削除し、Userクラスの初期化を修正

--- app/repository/UserRepository.py
+++ app/repository/UserRepository.py
@@ -1,6 +1,5 @@
 import datetime
 import uuid
-# from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.domain.User import User
@@ -8,20 +7,17 @@ from app.domain.User import User
 class UserRepository:
   def __init__(self, db: AsyncSession) -> None:
     self.db = db
-    self.user_id = ''
 
   async def register_user(self) -> str | None:
     try:
       user_id = uuid.uuid4().hex
       created_at = datetime.datetime.now(datetime.timezone.utc)
-      token = 'test_token'
 
-      user = User(user_id, created_at, token)
+      user = User(user_id, created_at)
       self.db.add(user)
 
       await self.db.flush()
 
-      self.user_id = user_id
       return user_id
     except Exception as e:
       print(f"Can not register user, {e}")
