92b5f344516f52aafcd74e8bca55c9cf6039e03f
hanzzoo <kou435@gmail.com>
2025-07-20 12:45:23 +0900

    feat(schemas): CartItemSchemaを追加し、GetCartItemResponseのレスポンスを更新

--- app/schemas.py
+++ app/schemas.py
@@ -1,5 +1,5 @@
-from pydantic import BaseModel
-from typing import List, Optional
+from pydantic import BaseModel, ConfigDict
+from typing import Any, List, Optional
 
 
 class ProductResponse(BaseModel):
@@ -19,3 +19,13 @@ class CartProductRequest(BaseModel):
 
 class UpdateToCartRequest(BaseModel):
     products: List[CartProductRequest]
+
+class CartItemSchema(BaseModel):
+    product_id: str
+    quantity: int
+    added_at: str
+
+    model_config = ConfigDict(from_attributes=True)
+
+class GetCartItemResponse(BaseModel):
+    products: List[CartItemSchema] |  List[Any]
