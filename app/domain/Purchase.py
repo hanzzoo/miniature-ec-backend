569f4cb9e4e8ca7573fd6cb8d869d8150087fb83
hanzzoo <kou435@gmail.com>
2025-07-21 22:57:25 +0900

    feat(domain): Purchaseクラスにtotal_amountカラムを追加し、コンストラクタを更新

--- app/domain/Purchase.py
+++ app/domain/Purchase.py
@@ -1,5 +1,5 @@
 import datetime
-from sqlalchemy import Column, DateTime, String
+from sqlalchemy import Column, DateTime, Integer, String
 from sqlalchemy.orm import declarative_base
 
 Base = declarative_base()
@@ -9,4 +9,12 @@ class Purchase(Base):
 
     purchase_id = Column(String, primary_key=True)
     user_id = Column(String, index=True)
+    total_amount = Column(Integer, index=True)
     purchased_at = Column(DateTime, default=datetime.datetime.now)
+
+
+    def __init__(self, purchase_id: str, user_id: str, total_amount: int, purchased_at: datetime.datetime):
+        self.purchase_id = purchase_id
+        self.user_id = user_id
+        self.total_amount = total_amount
+        self.purchased_at = purchased_at
