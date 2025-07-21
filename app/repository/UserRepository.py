e51727856d1c581d46a67da1c7ed9f0f677d3b36
hanzzoo <kou435@gmail.com>
2025-07-21 10:05:01 +0900

    feat(user): ユーザー登録メソッドにユーザー名、メール、パスワードの検証を追加し、パスワードをハッシュ化する処理を実装

--- app/repository/UserRepository.py
+++ app/repository/UserRepository.py
@@ -1,5 +1,6 @@
 import datetime
 import uuid
+import bcrypt
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.domain.User import User
@@ -8,12 +9,17 @@ class UserRepository:
   def __init__(self, db: AsyncSession) -> None:
     self.db = db
 
-  async def register_user(self) -> str | None:
+  async def register_user(self,user_name: str, user_email: str, user_password: str) -> str | None:
+    if not user_name or not user_email or not user_password:
+      raise ValueError('user_name or user_email or user_password is empty value')
     try:
       user_id = uuid.uuid4().hex
       created_at = datetime.datetime.now(datetime.timezone.utc)
+      hashed_user_password = bcrypt.hashpw(user_email.encode(), bcrypt.gensalt())
 
-      user = User(user_id, created_at)
+      #TODO - 検証：# 検証時　bcrypt.checkpw(plain_password.encode(), hashed_from_db.encode())
+
+      user = User(user_id, user_name, user_email, hashed_user_password.decode(), created_at)
       self.db.add(user)
 
       await self.db.flush()
