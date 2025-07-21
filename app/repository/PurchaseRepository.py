4654c947a2427be0fbefe88290b5c7bb82fa93ab
hanzzoo <kou435@gmail.com>
2025-07-21 22:57:30 +0900

    feat(repository): orderメソッドにtotal_amount引数を追加し、Purchaseインスタンスの生成を更新

--- app/repository/PurchaseRepository.py
+++ app/repository/PurchaseRepository.py
@@ -8,11 +8,11 @@ class PurchaseRepository:
     def __init__(self, db: AsyncSession) -> None:
        self.db = db
     
-    async def order(self, user_id: str) -> str:
+    async def order(self, user_id: str, total_amount: int) -> str:
        purchase_id = uuid.uuid4().hex
        purchased_at = datetime.datetime.now(datetime.timezone.utc)
 
-       purchase = Purchase(purchase_id=purchase_id, user_id=user_id, purchased_at=purchased_at)
+       purchase = Purchase(purchase_id, user_id, total_amount, purchased_at)
        self.db.add(purchase)
        
        await self.db.flush()
